467b5ab818c974a9f717a9fbb2793b590174ada0
PME Community <progmadeeasy@gmail.com>
2023-01-22 22:52:15 +0100

    Add project files.

---
+++ Services/SomeService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceLifeTime.Services
+{
+    public class SomeService: ITransientService, IScopedService, ISingletonService
+    {
+        Guid id;
+        public SomeService()
+        {
+            id = Guid.NewGuid();
+        }
+
+        public Guid GetID()
+        {
+            return id;
+        }
+    }
+
+
+    public interface ITransientService
+    {
+        Guid GetID();
+    }
+
+    public interface IScopedService
+    {
+        Guid GetID();
+    }
+
+    public interface ISingletonService
+    {
+        Guid GetID();
+    }
+
+}
