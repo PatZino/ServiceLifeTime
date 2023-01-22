9c3833fc8b61957215cd504c7bc8a95b89ab2b61
PatZino <eedawarez@gmail.com>
2023-01-22 23:34:51 +0100

    Code cleanup

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,6 @@ namespace ServiceLifeTime
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<ITransientService, SomeService>();
@@ -30,7 +29,6 @@ namespace ServiceLifeTime
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
